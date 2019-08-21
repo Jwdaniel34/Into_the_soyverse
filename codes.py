22ffd4a205a65dedc0e73398c0a81455da731b1b
John <John.daniel994@gmail.com>
2019-08-21 13:35:45 -0400

    added agg columns and examined education

--- codes.py
+++ codes.py
@@ -60,3 +60,82 @@ occupation = {'OCCULIS1':['Administrator, manager',
             'Armed forces']
                 }
 
+meats = {'030110':'Ground beef',
+'030210':'Chuck roast',
+'030310':'Round roast',
+'030410':'Other roast',
+'030510':'Round steak',
+'030610':'Sirloin steak',
+'030710':'Other steak',
+'030810':'Other beef',
+'040110':'Bacon',
+'040210':'Pork chops',
+'040310':'Ham canned',
+'040410':'Other pork',
+'040510':'Sausage',
+'040610':'Canned ham',
+'050110':'Frankfurters',
+'050210':'Bologna, liverwurst, salami',
+'050310':'Other lunchmeats',
+'050410':'Lamb organ meats',
+'050900':'Goat',
+'060110':'Fresh frozen whole chicken',
+'060210':'Fresh frozen chicken parts',
+'060310':'Other poultry',
+'070110':'Canned fish seafood',
+'070230':'Fresh fish shellfish',
+'070240':'Frozen fish shellfish'}
+
+income = {'01':'0-4,999',
+'02':'5,000-9,999',
+'03':'10,000-14,999',
+'04':'15,000-19,999',
+'05':'20,000-29,999',
+'06':'30,000-39,999',
+'07':'40,000-49,999',
+'08':'50,000-69,999',
+'09':'70,000-89,999',
+'10':'90,000-119,999',
+'11':'120,000 and over'}
+
+region ={
+    '1':'Northeast',
+'2':'Midwest',
+'3':'South',
+'4':'West'
+}
+
+housing = {
+    '1':"Homeowner w/ Mortgage",
+'2':"Homeowner w/o Mortgage ",
+'3':"Homeowner-Mortgage status not reported",
+'4':"Renter ",
+'5':"Occupied without cash payment",
+'6':"Student Housing"
+}
+
+refgen = {
+    '1':'Birth year of 1928 or earlier - Greatest generation (G.I. Generation)',
+'2':'Birth year from 1929 to 1945 - Silent generation',
+'3':'Birth year from 1946 to 1964 - Baby boomers',
+'4':'Birth year from 1965 to 1980 - also known as Generation X',
+'5':'Birth year of 1981 or later.  Sometimes referred to as the Millennials',
+}
+
+marital = {
+    '1':'Married',
+'2':'Widowed',
+'3':'Divorced',
+'4':'Separated',
+'5':'Never married'
+}
+
+education = {'EDU_00':'No_schooling',
+'EDU_10':'Elementary_school',
+'EDU_11':'Some_high_school',
+'EDU_12':'High_school_graduate',
+'EDU_13':'Some_college',
+'EDU_14':'Associate_degree',
+'EDU_15':'Bachelor_degree',
+'EDU_16':'Graduate_degree'
+}
